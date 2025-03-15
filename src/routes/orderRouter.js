645d731e8b6452264b84f631e050ee2174540a4c
Keaton Folsom <89952496+folskeat@users.noreply.github.com>
2025-03-15 12:38:47 -0600

    stop using routes

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -3,7 +3,7 @@ const config = require("../config.js");
 const { Role, DB } = require("../database/database.js");
 const { authRouter } = require("./authRouter.js");
 const { asyncHandler, StatusCodeError } = require("../endpointHelper.js");
-const Metrics = require("../metrics.js");
+//const Metrics = require("../metrics.js");
 
 const orderRouter = express.Router();
 
